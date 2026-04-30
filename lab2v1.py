2d6b01e3782821df4de4a0748c6a05f933e63476
mikegushchin333-create <mikegushchin333@gmail.com>
2026-04-30 11:27:31 +0300

    Update lab2v1.py
    исправил исправление на исправленную версию

--- lab2v1.py
+++ lab2v1.py
@@ -1,5 +1,7 @@
+import os
 def func(filepath):
-    import os
+    if not os.path.isabs(filepath):
+        filepath = os.path.join(os.path.dirname(os.path.abspath(__file__)), filepath)
     from Bio import SeqIO
     from Bio.SeqUtils import gc_fraction
     numb = 0
@@ -7,19 +9,18 @@ def func(filepath):
         for text_string in text:
             if text_string.startswith(">"):
                 numb += 1
-            if numb>10:
+            if numb > 10:
                 return
-        if len(SeqIO.parse(filepath).seq) > 1024:
+        if any(len(rec.seq) > 1024 for rec in SeqIO.parse(filepath, "fasta")):
             return
-        else:
-            seqs=list(SeqIO.parse(filepath,"fasta"))
-            gc = 0
-            gcid = 0
-            for i in range(len(seqs)):
-                gc_new = gc_fraction(seqs[i].seq)
-                if gc_new>gc:
-                    gc=gc_new
-                    gcid=i
+        seqs = list(SeqIO.parse(filepath, "fasta"))
+        gc = 0
+        gcid = 0
+        for i in range(len(seqs)):
+            gc_new = gc_fraction(seqs[i].seq)
+            if gc_new > gc:
+                gc = gc_new
+                gcid = i
     print(f"{seqs[gcid].id}\n{gc*100:.6f}")
-
-os.path.splitext("IN 2.txt")
+    
+func("IN 2.txt") 
