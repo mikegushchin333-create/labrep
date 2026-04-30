ab62512c5a6ed6cbe795b8d8dd2e210d9c2b532d
mikegushchin333-create <mikegushchin333@gmail.com>
2026-04-30 11:29:55 +0300

    Add files via upload

---
+++ Lab2v1/lab2v1.py
@@ -0,0 +1,24 @@
+def func(filepath):
+    from Bio import SeqIO
+    from Bio.SeqUtils import gc_fraction
+    numb = 0
+    with open(filepath, 'r', encoding='utf-8') as text:
+        for text_string in text:
+            if text_string.startswith(">"):
+                numb += 1
+            if numb>10:
+                return
+        if len(SeqIO.parse(filepath).seq) > 1024:
+            return
+        else:
+            seqs=list(SeqIO.parse(filepath,"fasta"))
+            gc = 0
+            gcid = 0
+            for i in range(len(seqs)):
+                gc_new = gc_fraction(seqs[i].seq)
+                if gc_new>gc:
+                    gc=gc_new
+                    gcid=i
+    print(f"{seqs[gcid].id}\n{gc*100:.6f}")
+    
+func("C:\\Users\\user\\Downloads\\IN 2.txt")
